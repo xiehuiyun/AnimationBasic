96915e07427a9f8f5d445dcdfd42766e6f69260f
NataliePortman <1401010800@qq.com>
2017-09-13 10:33:44 +0800

    完成设置Interpolator(插值器)的demo，添加备注

--- app/src/main/java/com/hencoder/hencoderpracticedraw6/practice/Practice07Interpolator.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw6/practice/Practice07Interpolator.java
@@ -52,23 +52,24 @@ public class Practice07Interpolator extends LinearLayout {
     }
 
     {
+        //画一个X轴（时间完成度）向右为正，y轴（动作完成度）向上为正的坐标。画出的图形是插值器的
         interpolatorPath = new Path();
         interpolatorPath.lineTo(0.25f, 0.25f);
         interpolatorPath.moveTo(0.25f, 1.5f);
         interpolatorPath.lineTo(1, 1);
-        interpolators[0] = new AccelerateDecelerateInterpolator();
-        interpolators[1] = new LinearInterpolator();
-        interpolators[2] = new AccelerateInterpolator();
-        interpolators[3] = new DecelerateInterpolator();
-        interpolators[4] = new AnticipateInterpolator();
-        interpolators[5] = new OvershootInterpolator();
-        interpolators[6] = new AnticipateOvershootInterpolator();
-        interpolators[7] = new BounceInterpolator();
-        interpolators[8] = new CycleInterpolator(0.5f);
-        interpolators[9] = PathInterpolatorCompat.create(interpolatorPath);
-        interpolators[10] = new FastOutLinearInInterpolator();
-        interpolators[11] = new FastOutSlowInInterpolator();
-        interpolators[12] = new LinearOutSlowInInterpolator();
+        interpolators[0] = new AccelerateDecelerateInterpolator();//先加速再减速
+        interpolators[1] = new LinearInterpolator();//匀速
+        interpolators[2] = new AccelerateInterpolator();//持续加速
+        interpolators[3] = new DecelerateInterpolator();//持续减速直到 0
+        interpolators[4] = new AnticipateInterpolator();//先回拉一下再进行正常动画轨迹,弹射一样的先压缩再发射
+        interpolators[5] = new OvershootInterpolator();//动画会超过目标值一些，然后再弹回来
+        interpolators[6] = new AnticipateOvershootInterpolator();//开始前回拉，最后超过一些然后回弹
+        interpolators[7] = new BounceInterpolator();//在目标值处弹跳。有点像玻璃球掉在地板上的效果
+        interpolators[8] = new CycleInterpolator(0.5f);//这个也是一个正弦 / 余弦曲线。0.5为一个来回。
+        interpolators[9] = PathInterpolatorCompat.create(interpolatorPath);//自己设置插值器
+        interpolators[10] = new FastOutLinearInInterpolator();//加速运动，它和 AccelerateInterpolator 一样，它俩最主要的区别是 FastOutLinearInInterpolator 的初始阶段加速度比 AccelerateInterpolator 要快一些。
+        interpolators[11] = new FastOutSlowInInterpolator();//先加速再减速
+        interpolators[12] = new LinearOutSlowInInterpolator();//持续减速
     }
 
     @Override
